34b9b2e80b17911669faadb96aa96381a8e58af2
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-02-08 19:46:06 +0100

    Removed broken __about__ import
    m)

--- aerofiles/__init__.py
+++ aerofiles/__init__.py
@@ -1,10 +1,5 @@
 # flake8: noqa
 
-from .__about__ import (
-    __title__, __summary__, __uri__, __version__, __author__, __email__,
-    __license__,
-)
-
 from .errors import *
 
 from .seeyou import (
