093cd37edaeaefc819681f81f38aa7f7ce2581d9
GliderGeek <GliderGeek@users.noreply.github.com>
2016-06-12 22:25:38 +0200

    Adding functionality of reading task part of .cup file

--- aerofiles/seeyou/reader.py
+++ aerofiles/seeyou/reader.py
@@ -193,3 +193,82 @@ class Reader:
             return None
 
         return description
+
+    def decode_taskname(self, fields):
+        return fields[0]
+
+    def decode_tasklist(self, fields, waypoints):
+        task_list = fields[1::]
+        task = []
+
+        for index in range(len(task_list)):
+            for waypoint in waypoints:
+                if waypoint["name"] == task_list[index]:
+                    taskpoint = {
+                        "ObsZone": index-1,  # ObsZone take-off = -1
+                        "name": waypoint["name"],
+                        "latitude": waypoint["latitude"],
+                        "longitude": waypoint["longitude"]
+                    }
+                    task.append(taskpoint)
+                    break
+
+        return task
+
+    def decode_options(self, fields):
+        if not fields[0] == "Options":
+            return
+
+        task_options = {
+            "TaskTime": None,
+            "WpDis": False,
+            "MinDis": False,
+            "RandomOrder": False,
+            "MaxPts": None
+        }
+
+        for field in fields:
+            if field == "Options":
+                continue
+            elif field.split("=")[0] in ["TaskTime"]:                                # string
+                task_options[field.split("=")[0]] = field.split("=")[1]
+            elif field.split("=")[0] in ["WpDis", "MinDis", "RandomOrder"]:          # boolean
+                task_options[field.split("=")[0]] = bool(field.split("=")[1])
+            elif field.split("=")[0] in ["MaxPts"]:                                  # int
+                task_options[field.split("=")[0]] = int(field.split("=")[1])
+            else:
+                raise Exception("Input contains unsupported option %s" % field)
+
+        return task_options
+
+    def decode_taskpoint(self, fields, task):
+
+        if len(task) == 0:
+            raise ValueError("Task is empty, but should filled with taskpoints")
+
+        taskpoint_info = {
+            "Style": None,
+            "R1": None,
+            "A1": None,
+            "R2": None,
+            "A2": None,
+            "A12": None,
+            "Line": False,
+            "Move": False,
+            "Reduce": False
+        }
+
+        ObsZone = None
+        for field in fields:
+            if field.split("=")[0] == "ObsZone":
+                ObsZone = int(field.split("=")[1])
+            elif field.split("=")[0] in ["Style", "A1", "A2", "A12"]:
+                taskpoint_info[field.split("=")[0]] = field.split("=")[1]
+            elif field.split("=")[0] in ["R1", "R2"]:
+                taskpoint_info[field.split("=")[0]] = field.split("=")[1][0:-1]  # taking off m
+            elif (field.split("=")[0] in ["Line", "Move", "Reduce"]) and (field.split("=")[1] == "1"):
+                taskpoint_info[field.split("=")[0]] = True
+            else:
+                raise Exception("A taskpoint does not contain key %s" % field.split("=")[0])
+
+        return task[ObsZone+1].update(taskpoint_info)
