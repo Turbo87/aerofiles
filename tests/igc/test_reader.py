cddf6dfb403df61ec6954374e71ea3ee260cedad
GliderGeek <GliderGeek@users.noreply.github.com>
2017-07-29 16:50:27 +0200

    fix wrong tests, containing record type

--- tests/igc/test_reader.py
+++ tests/igc/test_reader.py
@@ -6,145 +6,108 @@ from aerofiles.igc.reader import LowLevelReader
 
 
 def test_decode_A_record():
-
     line = 'AXXXABC FLIGHT:1\r\n'
     expected_result = {
-        'type': 'A',
-        'value': {
-            'manufacturer': 'XXX',
-            'id': 'ABC',
-            'id_addition': 'FLIGHT:1'
-        }
+        'manufacturer': 'XXX',
+        'id': 'ABC',
+        'id_addition': 'FLIGHT:1'
     }
 
     assert LowLevelReader.decode_A_record(line) == expected_result
 
 
 def test_decode_B_record():
-
     line = 'B1602455107126N00149300WA002880042919509020\r\n'
     expected_result = {
-        'type': 'B',
-        'value': {
-            'time': datetime.time(16, 2, 45),
-            'lat': 51.118766666666666,
-            'lon': -1.8216666666666668,
-            'validity': 'A',
-            'pressure_alt': 288,
-            'gps_alt': 429,
-            'start_index_extensions': 35,
-            'extensions_string': '19509020'
-        }
+        'time': datetime.time(16, 2, 45),
+        'lat': 51.118766666666666,
+        'lon': -1.8216666666666668,
+        'validity': 'A',
+        'pressure_alt': 288,
+        'gps_alt': 429,
+        'start_index_extensions': 35,
+        'extensions_string': '19509020'
     }
 
     assert LowLevelReader.decode_B_record(line) == expected_result
 
 
 def test_decode_C_record1():
-
     line = 'C150701213841160701000102 500K Tri\r\n'
     expected_result = {
-        'type': 'C',
-        'value': {
-            'subtype': 'task_info',
-            'declaration_date': datetime.date(2001, 07, 15),
-            'declaration_time': datetime.time(21, 38, 41),
-            'flight_date': datetime.date(2001, 07, 16),
-            'number': '0001',
-            'num_turnpoints': 2,
-            'description': '500K Tri'
-        }
+        'subtype': 'task_info',
+        'declaration_date': datetime.date(2001, 07, 15),
+        'declaration_time': datetime.time(21, 38, 41),
+        'flight_date': datetime.date(2001, 07, 16),
+        'number': '0001',
+        'num_turnpoints': 2,
+        'description': '500K Tri'
     }
 
     assert LowLevelReader.decode_C_record(line) == expected_result
 
 
 def test_decode_C_record2():
-
     line = 'C5111359N00101899W Lasham Clubhouse\r\n'
     expected_result = {
-        'type': 'C',
-        'value': {
-            'subtype': 'waypoint_info',
-            'latitude': 51.18931666666667,
-            'longitude': -1.03165,
-            'description': 'Lasham Clubhouse'
-        }
+        'subtype': 'waypoint_info',
+        'latitude': 51.18931666666667,
+        'longitude': -1.03165,
+        'description': 'Lasham Clubhouse'
     }
 
     assert LowLevelReader.decode_C_record(line) == expected_result
 
 
 def test_decode_D_record():
-
     line = 'D20331\r\n'
     expected_result = {
-        'type': 'D',
-        'value': {
-            'qualifier': 'DGPS',
-            'station_id': '0331'
-        }
+        'qualifier': 'DGPS',
+        'station_id': '0331'
     }
 
     assert LowLevelReader.decode_D_record(line) == expected_result
 
 
 def test_decode_E_record():
-
     line = 'E160245PEV\r\n'
     expected_result = {
-        'type': 'E',
-        'value': {
-            'time': datetime.time(16, 02, 45),
-            'tlc': 'PEV',
-            'extension_string': ''
-        }
+        'time': datetime.time(16, 02, 45),
+        'tlc': 'PEV',
+        'extension_string': ''
     }
 
     assert LowLevelReader.decode_E_record(line) == expected_result
 
 
 def test_decode_F_record():
-
     line = 'F160240040609123624221821\r\n'
     expected_result = {
-        'type': 'F',
-        'value': {
-            'time': datetime.time(16, 02, 40),
-            'satelites': ['04', '06', '09', '12', '36', '24', '22', '18', '21']
-        }
+        'time': datetime.time(16, 02, 40),
+        'satelites': ['04', '06', '09', '12', '36', '24', '22', '18', '21']
     }
 
     assert LowLevelReader.decode_F_record(line) == expected_result
 
 
 def test_decode_G_record():
-
     line = 'GREJNGJERJKNJKRE31895478537H43982FJN9248F942389T433T\r\n'
-    expected_result = {
-        'type': 'G',
-        'value': 'REJNGJERJKNJKRE31895478537H43982FJN9248F942389T433T'
-    }
+    expected_result = 'REJNGJERJKNJKRE31895478537H43982FJN9248F942389T433T'
 
     assert LowLevelReader.decode_G_record(line) == expected_result
 
 
 def test_decode_H_record():
-
     line = 'HFFXA035\r\n'
     expected_result = {
-        'type': 'H',
-        'value': {
-            'source': 'F',
-            'fix_accuracy': 35
-        }
+        'source': 'F',
+        'fix_accuracy': 35
     }
 
     assert LowLevelReader.decode_H_record(line) == expected_result
 
 
 def test_decode_H_utc_date():
-
     line = 'HFDTE160701\r\n'
     expected_result = {
         'utc_date': datetime.date(2001, 07, 16)
@@ -154,7 +117,6 @@ def test_decode_H_utc_date():
 
 
 def test_decode_H_pilot():
-
     line = 'HFPLTPILOTINCHARGE: Bloggs Bill D\r\n'
     expected_result = {
         'pilot': 'Bloggs Bill D'
@@ -164,7 +126,6 @@ def test_decode_H_pilot():
 
 
 def test_decode_H_copilot():
-
     line = 'HFCM2CREW2: Smith-Barry John A\r\n'
     expected_result = {
         'copilot': 'Smith-Barry John A'
@@ -174,7 +135,6 @@ def test_decode_H_copilot():
 
 
 def test_decode_H_glider_model():
-
     line = 'HFGTYGLIDERTYPE: Schleicher ASH-25\r\n'
     expected_result = {
         'glider_model': 'Schleicher ASH-25'
@@ -184,7 +144,6 @@ def test_decode_H_glider_model():
 
 
 def test_decode_H_glider_registration():
-
     line = 'HFGIDGLIDERID: ABCD-1234\r\n'
     expected_result = {
         'glider_registration': 'ABCD-1234'
@@ -194,7 +153,6 @@ def test_decode_H_glider_registration():
 
 
 def test_decode_H_gps_datum():
-
     line = 'HFDTM100GPSDATUM: WGS-1984\r\n'
     expected_result = {
         'gps_datum': 'WGS-1984'
@@ -204,7 +162,6 @@ def test_decode_H_gps_datum():
 
 
 def test_decode_H_firmware_revision():
-
     line = 'HFRFWFIRMWAREVERSION:6.4\r\n'
     expected_result = {
         'firmware_revision': '6.4'
@@ -214,7 +171,6 @@ def test_decode_H_firmware_revision():
 
 
 def test_decode_H_hardware_revision():
-
     line = 'HFRHWHARDWAREVERSION:3.0\r\n'
     expected_result = {
         'hardware_revision': '3.0'
@@ -224,9 +180,8 @@ def test_decode_H_hardware_revision():
 
 
 def test_decode_H_manufacturer_model():
-
     line = 'HFFTYFRTYPE: Manufacturer, Model\r\n'
-    expected_result={
+    expected_result = {
         'logger_manufacturer': 'Manufacturer',
         'logger_model': 'Model'
     }
@@ -235,7 +190,6 @@ def test_decode_H_manufacturer_model():
 
 
 def test_decode_H_gps_receiver():
-
     line = 'HFGPS:MarconiCanada, Superstar, 12ch, max10000m\r\n'
     expected_result = {
         'gps_manufacturer': 'MarconiCanada',
@@ -251,7 +205,6 @@ def test_decode_H_gps_receiver():
 
 
 def test_decode_H_gps_receiver2():
-
     line = 'HFGPS:GLOBALTOP,FGPMMOPA6,66,max18000m\r\n'
     expected_result = {
         'gps_manufacturer': 'GLOBALTOP',
@@ -267,7 +220,6 @@ def test_decode_H_gps_receiver2():
 
 
 def test_decode_H_gps_receiver3():
-
     line = 'HFGPS:GlobalTopPA6B,66ch,max18000m\r\n'
     expected_result = {
         'gps_manufacturer': 'GlobalTopPA6B',
@@ -283,7 +235,6 @@ def test_decode_H_gps_receiver3():
 
 
 def test_decode_H_gps_receiver4():
-
     line = 'HFGPS:UBLOX,NEO-6G,16Ch,50000\r\n'
     expected_result = {
         'gps_manufacturer': 'UBLOX',
@@ -299,19 +250,18 @@ def test_decode_H_gps_receiver4():
 
 
 def test_decode_H_gps_receiver5():
-
-        line = 'HFGPS:LX\r\n'
-        expected_result = {
-            'gps_manufacturer': 'LX',
-            'gps_model': None,
-            'gps_channels': None,
-            'gps_max_alt': {
-                'value': None,
-                'unit': None
-            }
+    line = 'HFGPS:LX\r\n'
+    expected_result = {
+        'gps_manufacturer': 'LX',
+        'gps_model': None,
+        'gps_channels': None,
+        'gps_max_alt': {
+            'value': None,
+            'unit': None
         }
+    }
 
-        assert LowLevelReader.decode_H_gps_receiver(line) == expected_result
+    assert LowLevelReader.decode_H_gps_receiver(line) == expected_result
 
 
 def test_decode_H_gps_receiver6():
@@ -330,7 +280,6 @@ def test_decode_H_gps_receiver6():
 
 
 def test_decode_H_pressure_sensor():
-
     line = 'HFPRSPRESSALTSENSOR: Sensyn, XYZ1111, max11000m\r\n'
     expected_result = {
         'pressure_sensor_manufacturer': 'Sensyn',
@@ -345,7 +294,6 @@ def test_decode_H_pressure_sensor():
 
 
 def test_decode_H_pressure_sensor2():
-
     line = 'HFPRSPressAltSensor:Intersema MS5534B,8191\r\n'
     expected_result = {
         'pressure_sensor_manufacturer': 'Intersema',
@@ -360,7 +308,6 @@ def test_decode_H_pressure_sensor2():
 
 
 def test_decode_H_competition_id():
-
     line = 'HFCIDCOMPETITIONID: XYZ-78910\r\n'
     expected_result = {
         'competition_id': 'XYZ-78910'
@@ -370,7 +317,6 @@ def test_decode_H_competition_id():
 
 
 def test_decode_H_competition_class():
-
     line = 'HFCCLCOMPETITIONCLASS:15m Motor Glider\r\n'
     expected_result = {
         'competition_class': '15m Motor Glider'
@@ -380,7 +326,6 @@ def test_decode_H_competition_class():
 
 
 def test_decode_H_time_zone_offset():
-
     line = 'HFTZNTIMEZONE:3\r\n'
     expected_result = {
         'time_zone_offset': 3
@@ -390,7 +335,6 @@ def test_decode_H_time_zone_offset():
 
 
 def test_decode_H_time_zone_offset2():
-
     line = 'HFTZNTIMEZONE:11.00\r\n'
     expected_result = {
         'time_zone_offset': 11
@@ -400,7 +344,6 @@ def test_decode_H_time_zone_offset2():
 
 
 def test_decode_H_mop_sensor():
-
     line = 'HFMOPSENSOR:MOP-(SN:1,ET=1375,0,1375,0,3.05V,p=0),Ver:0\r\n'
     expected_result = {
         'mop_sensor': 'MOP-(SN:1,ET=1375,0,1375,0,3.05V,p=0),Ver:0'
@@ -410,7 +353,6 @@ def test_decode_H_mop_sensor():
 
 
 def test_decode_H_site():
-
     line = 'HFSITSite: lk15comp\r\n'
     expected_result = {
         'site': 'lk15comp'
@@ -420,7 +362,6 @@ def test_decode_H_site():
 
 
 def test_decode_H_units_of_measure():
-
     line = 'HFUNTUnits: km,ft,kt'
     expected_result = {
         'units_of_measure': ['km', 'ft', 'kt']
@@ -430,57 +371,41 @@ def test_decode_H_units_of_measure():
 
 
 def test_decode_I_record():
-
     line = 'I033638FXA3940SIU4143ENL\r\n'
-    expected_result = {
-        'type': 'I',
-        'value': [
-            {'bytes': (36, 38), 'extension_type': 'FXA'},
-            {'bytes': (39, 40), 'extension_type': 'SIU'},
-            {'bytes': (41, 43), 'extension_type': 'ENL'}
-        ]
-    }
+    expected_result = [
+        {'bytes': (36, 38), 'extension_type': 'FXA'},
+        {'bytes': (39, 40), 'extension_type': 'SIU'},
+        {'bytes': (41, 43), 'extension_type': 'ENL'}
+    ]
 
     assert LowLevelReader.decode_I_record(line) == expected_result
 
 
 def test_decode_J_record():
-
     line = 'J010812HDT\r\n'
-    expected_result = {
-        'type': 'J',
-        'value': [
-            {'bytes': (8, 12), 'extension_type': 'HDT'}
-        ]
-    }
+    expected_result = [
+        {'bytes': (8, 12), 'extension_type': 'HDT'}
+    ]
 
     assert LowLevelReader.decode_J_record(line) == expected_result
 
 
 def test_decode_K_record():
-
     line = 'K16024800090\r\n'
     expected_result = {
-        'type': 'K',
-        'value': {
-            'time': datetime.time(16, 02, 48),
-            'value_string': '00090',
-            'start_index': 7
-        }
+        'time': datetime.time(16, 02, 48),
+        'value_string': '00090',
+        'start_index': 7
     }
 
     assert LowLevelReader.decode_K_record(line) == expected_result
 
 
 def test_decode_L_record():
-
     line = 'LXXXRURITANIAN STANDARD NATIONALS DAY 1\r\n'
     expected_result = {
-        'type': 'L',
-        'value': {
-            'source': 'XXX',
-            'comment': 'RURITANIAN STANDARD NATIONALS DAY 1'
-        }
+        'source': 'XXX',
+        'comment': 'RURITANIAN STANDARD NATIONALS DAY 1'
     }
 
     assert LowLevelReader.decode_L_record(line) == expected_result
@@ -492,7 +417,6 @@ def test_decode_latitude():
 
 
 def test_decode_longitude():
-
     assert LowLevelReader.decode_longitude('00657383E') == 6.956383333333333
     assert LowLevelReader.decode_longitude('09942706W') == -99.71176666666666
 
