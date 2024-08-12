c164650759bac87a87b945be3373952090a38d81
Dr. Tilmann Bubeck <tilmann@bubecks.de>
2024-08-12 15:40:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 
 setup(
     name='aerofiles',
-    version='1.2.1',
+    version='1.3.1',
     description='waypoint, task, tracklog readers and writers for aviation',
     long_description=read('README.rst'),
     url=GITHUB_URL,
