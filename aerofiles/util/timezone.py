62737bf0c4bc49e7e880bc372048db5555c19c37
Dr. Tilmann Bubeck <tilmann@bubecks.de>
2025-03-03 11:13:18 +0100

    Improve TimeZoneFix with dunder methods (#322)

--- aerofiles/util/timezone.py
+++ aerofiles/util/timezone.py
@@ -2,9 +2,32 @@ import datetime
 
 
 class TimeZoneFix(datetime.tzinfo):
+    """
+    A simple implementation of tzinfo to store timezone of IGC fixes.
 
-    def __init__(self, fix):
+    An alternative would be, to use "timezone" which is part of
+    Python since v3.2. However, because we try to be python 2.6+
+    compatible, we use this implementation. It can be replaced,
+    if we set Python 3.x as a minimum requirement for aerofiles.
+    """
+
+    def __init__(self, fix=0):
         self.fix = fix
+
+    def __eq__(self, other):
+        if isinstance(other, TimeZoneFix):
+            return self.fix == other.fix
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.fix)
+
+    def __repr__(self):
+        """Convert to formal string, for repr().
+        """
+        return "%s.%s(%r)" % (self.__class__.__module__,
+                              self.__class__.__qualname__,
+                              self.fix)
 
     def utcoffset(self, dt):
         return datetime.timedelta(hours=self.fix)
